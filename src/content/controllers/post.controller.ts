2de86e53729d0191abd74e1e6bb721e7f99be249
vhjihuang <1076891184@qq.com>
2023-09-08 23:23:48 +0800

    edit postcontroller add get, post, patch, delete methods

--- src/content/controllers/post.controller.ts
+++ src/content/controllers/post.controller.ts
@@ -11,7 +11,6 @@ import {
 import { isNil } from 'loadsh';
 
 import { PostEntity } from '../types';
-import { throws } from 'assert';
 
 let posts: PostEntity[] = [
   { title: '第一篇文章标题', body: '第一篇文章内容' },
@@ -34,5 +33,35 @@ export class PostController {
     const post = posts.find(v => v.id === Number(id));
     if (isNil(post)) throw new NotAcceptableException(`the post with id ${id} not exits!`)
     return post;
+  }
+
+
+  @Post()
+  async store() {
+    const newPost: PostEntity = {
+      id: Math.max(...posts.map(v => v.id +1 )),
+      title: '新增文章标题',
+      body: '新增文章内容'
+    };
+    posts.push(newPost);
+    return newPost;
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: number) {
+    let toUpdate = posts.find(v => v.id === Number(id));
+    if (isNil(toUpdate)) throw new NotAcceptableException(`the post with id ${id} not exits!`)
+    toUpdate = { ...toUpdate, title: '更新文章标题' };
+    posts = posts.map(v => v.id === Number(id) ? toUpdate : v);
+    return toUpdate;
+  }
+
+
+  @Delete(':id') 
+  async delete(@Param('id') id: number) {
+    const toDelete = posts.find(v => v.id === Number(id));
+    if (isNil(toDelete)) throw new NotAcceptableException(`the post with id ${id} not exits!`)
+    posts = posts.filter(v => v.id !== Number(id));
+    return toDelete;
   }
 }
